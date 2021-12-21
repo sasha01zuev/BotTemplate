7f8ec75f7795ce4be0f7b12be3882366437d0276
Alex <41638681+sasha01zuev@users.noreply.github.com>
2021-12-21 22:55:04 +0300

    Added blacklist function

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher
 from .throttling import ThrottlingMiddleware
+from .blacklist import SetBlacklist
 
 
 def setup(dp: Dispatcher):
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(SetBlacklist())
